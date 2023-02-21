a1f953537fa4f1fea9780c0063392cc787f72d56
user <0000>
2023-02-21 22:52:20 +0100

    created a color class

---
+++ lib/utils/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color secondaryColor = Color(0xff3fa9df);
+  static Color secondaryColor2 = const Color(0xff3fa9df).withOpacity(0.4);
+  static Color secondaryColor3 = const Color(0xff3fa9df).withOpacity(0.7);
+  static const Color greyColor2 = Color(0xFF48484B);
+  static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color whiteColor2 = Color(0xFFC8C8C8);
+  static const Color blackColor = Color(0xFF000000);
+  static const Color redColor = Color(0xFFED5757);
+}
